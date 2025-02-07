ab8e8ab6a4636c8414a3c0f34cce870f87a33d0e
Gabriel de Santana <joaog.santana2006@gmail.com>
2025-02-07 16:19:10 -0300

    Iniciando o desafio do mestre

--- Biblioteca/Classe/Usuario.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Biblioteca.Classe
-{
-    public class Usuario
-    {
-        public string? Nome { get; set; }
-        public string? Email { get; set; }
-        public int Idade { get; set; }
-    }
-}
---
+++ Biblioteca/Classes/Podcast.cs
@@ -0,0 +1,18 @@
+﻿namespace Biblioteca.Classes
+{
+    public class Podcast
+    {
+        private List<Epsodio> epsodios = new();
+
+        public Podcast(string nome, string apresentador)
+        {
+            Nome = nome;
+            Apresentador = apresentador;
+        }
+
+        public string Nome { get; }
+        public string Apresentador { get; }
+
+        public int DuracaoTotal => epsodios.Sum();
+    }
+}
